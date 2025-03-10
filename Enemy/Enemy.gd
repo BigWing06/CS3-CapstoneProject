f90cbae79b234e2d9ae2f6d741a521e226741c3c
jholl <199132361+jholl18@users.noreply.github.com>
2025-03-10 10:19:16 -0400

    Finished enemy movement and implemented to world

---
+++ Enemy/Enemy.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+@onready var enemy = $enemyBody
+
+var _enemyData = {}
+var _speed
+var _movementType
+var _target = Vector2(0, 0)
+var _startingHealth
+var _hybrid = false
+var _player
+
+var _resourceJSONPath = "res://Enemy/enemyTypes.json"
+var _resourceJSONText = FileAccess.get_file_as_string(_resourceJSONPath)
+var _resouceJSON = JSON.parse_string(_resourceJSONText)
+
+func _ready():
+	_enemyData = _resouceJSON
+	_update("polar_bear") #choose animal from json file
+
+func _physics_process(delta):
+	#changes target and specifies hybrid
+	if _movementType == "baseFocused":
+		_target = global.basePosition
+	elif _movementType == "playerFocused":
+		_target = global.playerPosition
+	else:
+		_target = global.basePosition
+		_hybrid = true 
+
+	enemy.position += (_target - enemy.position)/_speed #sets a target for enemy to follow
+	
+func _update(x): #updates enemy variables
+	_speed = _enemyData[x]["speed"]
+	_movementType = _enemyData[x]["movement"]
+	_startingHealth = _enemyData[x]["health"]
+
+func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if _hybrid:
+		_movementType = "playerFocused"
+
+func _on_area_2d_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if _hybrid:
+		_movementType = "baseFocused"
--- globals/globals.gd
+++ globals/globals.gd
@@ -1,3 +1,6 @@
 extends Node
 
 var resourceManager = preload("res://inventory/resourceManager.gd").new()
+var playerPosition = Vector2(0, 0) #placeholder so values are not null
+var basePosition = Vector2(500, 400) #would like to eventually have this set the position of the base in a world script
+ 
