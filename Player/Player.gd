be44878e5e3a89299d052a419c5c22b300f2268e
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2025-03-22 20:49:38 -0400

    Added Camera, Organized playermovement code

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,7 +6,7 @@ var screen_size
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func _process(delta):
+func processMovement(delta): #Proceses the player movement
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
@@ -16,9 +16,13 @@ func _process(delta):
 		velocity.y -= 1
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
-	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-	
 	position += velocity*delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+
+func _physics_process(delta: float) -> void:
+	processMovement(delta)
+
+func _process(delta):
+	pass
+	
