4b6a8a2851e86e358eec7cd91051d903ea10ba02
Andrew <198925701+Andrew-2772@users.noreply.github.com>
2025-03-25 08:11:17 -0400

    added start screen layout

---
+++ startscreen/start_screen.gd
@@ -0,0 +1,11 @@
+extends Control
+
+var screen_texture = preload("res://Player/screen.png")
+var square_texture = preload("res://Player/square.png")
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Player/player.tscn")
+
+
+func _on_end_button_pressed() -> void:
+	get_tree().quit()
