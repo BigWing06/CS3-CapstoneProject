e77091a0059eb77ce979ceca31a8be924751f977
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2025-03-26 08:34:19 -0400

    World generation (#34)
    * Added Camera, Organized playermovement code
    
    * Added Chunk variable to player script
    
    * Update to rendering (switch to chunk system, accounts for position)
    
    * Finished Chunk Generation around player
    
    * Creation of chunkChanged signal and efficiency improvements

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,20 @@
 extends CharacterBody2D
 
+signal chunkChanged
+
 @export var speed = 400
 var screen_size
+var _chunk: Vector2i
+var _preChunk: Vector2i = Vector2i(0,0) #Keeps track of the previous chunk the player was in
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	global.world.get_node("TileMaps").playerRenderNeighborChunks(getCurrentChunk())
 
-func _process(delta):
+func getCurrentChunk() -> Vector2i: #Returns the current chunk that the player is in
+	return global.world.get_node("TileMaps").getChunk(position)
+
+func _physics_process(delta: float) -> void:
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
@@ -16,9 +24,17 @@ func _process(delta):
 		velocity.y -= 1
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
-	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-	
 	position += velocity*delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+
+func _process(delta):
+	if (getCurrentChunk() != _chunk): #Determined if the player has entered a new chunk
+		_preChunk = _chunk
+		_chunk = getCurrentChunk()
+		chunkChanged.emit()
+		
+
+
+func _on_chunk_changed() -> void: #Run when the player enters a new chunk
+	global.world.get_node("TileMaps").playerRenderNeighborChunks(getCurrentChunk())
