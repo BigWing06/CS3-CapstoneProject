d5a9ce693b37770b188574483a8efa298411029b
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2025-03-26 22:15:50 -0400

    Implemented selection in tower building menu

--- inventory/craftingMenu/crafting_menu.gd
+++ inventory/craftingMenu/crafting_menu.gd
@@ -1,14 +1,30 @@
 extends Control
 
+signal selectedTowerChanged(tower)
+
 var _towerListScene = preload("res://inventory/craftingMenu/craftingMenuTowerListInstance.tscn") #Reference to scene for the tower menu list
 @onready var _towerDisplayList = $scrollContainer/towerDisplayList #Stores refence to towerDispalyList node for use later
 
 var _towerTypesJSONPath = "res://gameplayReferences/towerTypes.json"
 var _towerTypesJSONText = FileAccess.get_file_as_string(_towerTypesJSONPath)
 var _towerTypesJSON = JSON.parse_string(_towerTypesJSONText)
+var _towerTypesList = _towerTypesJSON.keys()
+var _selectedTowerType = 0
 
 func _ready() -> void:
-	for key in _towerTypesJSON.keys():
+	for key in _towerTypesList: #Instances the craftingMenuTowerListInstance to create the list in the crafting menu
 		var towerListSceneInstance = _towerListScene.instantiate()
-		towerListSceneInstance.update(_towerTypesJSON[key])
+		towerListSceneInstance.update(key, _towerTypesJSON[key])
+		selectedTowerChanged.connect(towerListSceneInstance.selectedTowerUpdate)
 		_towerDisplayList.add_child(towerListSceneInstance)
+	selectedTowerChanged.emit(_towerTypesList[_selectedTowerType])
+	
+func _process(delta: float) -> void:
+	#If statements to change the selected tower on mouse scroll
+	if Input.is_action_just_pressed("craftingScrollDown"):
+		_selectedTowerType = (_selectedTowerType + 1) % len(_towerTypesList)
+		selectedTowerChanged.emit(_towerTypesList[_selectedTowerType])
+	if Input.is_action_just_pressed("craftingScrollUp"):
+		_selectedTowerType = (_selectedTowerType - 1) % len(_towerTypesList)
+		selectedTowerChanged.emit(_towerTypesList[_selectedTowerType])
+	
--- inventory/craftingMenu/crafting_menu_tower_list_instance.gd
+++ inventory/craftingMenu/crafting_menu_tower_list_instance.gd
@@ -1,4 +1,16 @@
 extends Control
 
-func update(towerPlacementData): #This function is to be called by the ready functionn inn the crafting menu script
-	$HBoxContainer/Control/name.text = towerPlacementData['name']
+var _towerPlacementData
+var _towerType
+
+func update(tower, towerPlacementData): #This function is to be called by the ready functionn in the crafting menu script
+	_towerType = tower
+	_towerPlacementData = towerPlacementData
+	$HBoxContainer/Control/name.text = _towerPlacementData['name']
+	
+func selectedTowerUpdate(tower): #This function is to be connected to the selectedTowerChanged signal in the crafting menu scipt, it updates the instance to reflect which tower is being placed
+	if tower == _towerType:
+		$ColorRect.color = Color(1, 0, 0)
+	else:
+		$ColorRect.color = Color(0.14, 0.14, 0.14)
+	
