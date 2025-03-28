35d1885537a233c43e616ba7768c22a55988828f
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2025-03-27 23:20:20 -0400

    Made container scrollable, made menu visibilty toggleable

--- inventory/craftingMenu/crafting_menu.gd
+++ inventory/craftingMenu/crafting_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-signal selectedTowerChanged(tower)
+signal selectedTowerChanged(FOCUS_CLICK)
 
 var _towerListScene = preload("res://inventory/craftingMenu/craftingMenuTowerListInstance.tscn") #Reference to scene for the tower menu list
 @onready var _towerDisplayList = $scrollContainer/towerDisplayList #Stores refence to towerDispalyList node for use later
@@ -12,6 +12,7 @@ var _towerTypesList = _towerTypesJSON.keys()
 var _selectedTowerType = 0
 
 func _ready() -> void:
+	set_focus_mode(FOCUS_NONE)
 	for key in _towerTypesList: #Instances the craftingMenuTowerListInstance to create the list in the crafting menu
 		var towerListSceneInstance = _towerListScene.instantiate()
 		towerListSceneInstance.update(key, _towerTypesJSON[key])
@@ -27,4 +28,11 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("craftingScrollUp"):
 		_selectedTowerType = (_selectedTowerType - 1) % len(_towerTypesList)
 		selectedTowerChanged.emit(_towerTypesList[_selectedTowerType])
+	if Input.is_action_just_pressed("toggleBuildMenu"):
+		toggleMenu()
 	
+func _on_selected_tower_changed(tower: Variant) -> void:
+	$Control/towerTitle.text = _towerTypesJSON[tower]['name']
+		
+func toggleMenu():
+	visible = !visible
--- inventory/craftingMenu/crafting_menu_tower_list_instance.gd
+++ inventory/craftingMenu/crafting_menu_tower_list_instance.gd
@@ -7,10 +7,12 @@ func update(tower, towerPlacementData): #This function is to be called by the re
 	_towerType = tower
 	_towerPlacementData = towerPlacementData
 	$HBoxContainer/Control/name.text = _towerPlacementData['name']
+	focus_mode = FOCUS_ALL
+	
 	
 func selectedTowerUpdate(tower): #This function is to be connected to the selectedTowerChanged signal in the crafting menu scipt, it updates the instance to reflect which tower is being placed
 	if tower == _towerType:
 		$ColorRect.color = Color(1, 0, 0)
+		call_deferred("grab_focus")
 	else:
 		$ColorRect.color = Color(0.14, 0.14, 0.14)
-	
