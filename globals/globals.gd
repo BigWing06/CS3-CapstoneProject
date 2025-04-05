a6448db81bc106dbd3341715fe2d417e8dd4ffa0
GingerEdition22 <185860158+GingerEdition22@users.noreply.github.com>
2025-04-05 19:04:49 -0400

    Enemy attacks (#36)
    * Created health hearts
    
    * Created basic player damage and health bar
    
    * Added basic ranged and melee attacks and attacks dictionary
    
    * Begun to add multiple attack options
    
    * Added weighted attacks
    
    * Rotated projectile towards target
    
    * Added projectile documentation
    
    * Added damage taken indicator
    
    * Added in basic default attacks
    
    * Fixed heatlh change colors
    
    * Fixed health bar
    
    Removed the individual heart scene and rearranged the nodes into a new health bar scene and folder
    
    * Removed projectile sprite
    
    Temporarily removed the JSON link to the projectile sprite until an actual asset is created

--- globals/globals.gd
+++ globals/globals.gd
@@ -8,3 +8,4 @@ var resourceManager = preload("res://inventory/resourceManager.gd").new()
 var worldPath = "/root/Main/World" # the world node path
 var WORLD_SEED = randi_range(0,5000)
 var basePosition = Vector2(500, 400) #would like to eventually have this set the position of the base in a world script
+var attackTypes = JSON.parse_string(FileAccess.get_file_as_string("res://Enemy/Attacks/attacks.json")) ### This needs moved when we implement the JSON script
