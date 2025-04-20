925642c87edbf085d15fa0285bb3b94dd6153343
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2025-04-19 23:22:30 -0400

    Enemy droppings (#54)
    * Basic Animation Implementation
    
    * Addition of enemyAnimations, Turning of enemies, and fixed enemey attacking
    
    * Tweaked Lake generation to provide more natural generation
    
    * Added minimum empty distance around base
    
    * Added Collision Shapes to water
    
    * Added player animation
    
    * Added gameover scene and base death
    
    * Added player death
    
    * Added enemy despawning
    
    * Fix of player health change and cleannup for pull request
    
    * Addition of comments for pull request
    
    * Fix of merge errors
    
    * Creation of dropped item scene
    
    * Added dropping of items on enemy kill
    
    * Fix of changes after merge
    
    * Removal of print statement for merge

--- globals/spawner.gd
+++ globals/spawner.gd
@@ -6,4 +6,4 @@ func spawnEnemy(position): #Spawns an enemy at a certain position
 	var _enemy = _enemyScene.instantiate()
 	_enemy.position = position #Sets enemy position to the random position.
 	global.world.gameover.connect(_enemy.queue_free)
-	get_parent().add_child(_enemy)
+	global.world.add_child(_enemy)
