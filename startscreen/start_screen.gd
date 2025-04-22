0f4e288ef841ffa07aa2bf8a661e0e54401cb91d
GingerEdition22 <185860158+GingerEdition22@users.noreply.github.com>
2025-04-21 20:53:59 -0400

    Added start screen animations

--- startscreen/start_screen.gd
+++ startscreen/start_screen.gd
@@ -1,6 +1,10 @@
 extends Control
 
-
+func _ready() -> void:
+	for child in $Animations/Enemies.get_children():
+		child.sprite_frames = load("res://Enemy/Animations/"+utils.getEnemies().pick_random()+"animations.tres")
+	for child in get_tree().get_nodes_in_group("StartScreenAnimations"):
+		child.play()
 func _on_start_button_pressed() -> void:
 	AudioController.play_menu()
 	get_tree().change_scene_to_file("res://Viewport/Main.tscn")
