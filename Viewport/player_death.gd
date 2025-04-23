210ed4bcf5a0aadf8e17f8d2b3cc595511c8fd36
GingerEdition22 <185860158+GingerEdition22@users.noreply.github.com>
2025-04-22 23:40:15 -0400

    Added tower icons and fixed death screen visibility bug

--- Viewport/player_death.gd
+++ Viewport/player_death.gd
@@ -2,10 +2,13 @@ extends Control
 
 func _ready() -> void:
 	modulate = Color8(0,0,0,0)
+	visible=false
 func showDeathScreen():
+	visible=true
 	$AnimationPlayer.play("Fade")
 	$ColorRect/AnimatedSprite2D.play("walk")
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	$ColorRect/AnimatedSprite2D.stop()
+	visible=false
