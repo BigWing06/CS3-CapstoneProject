b2e524767d1e26160e06f2d1580497f107f2e183
GingerEdition22 <185860158+GingerEdition22@users.noreply.github.com>
2025-04-22 20:06:56 -0400

    Attempted Updates

--- Viewport/compass.gd
+++ Viewport/compass.gd
@@ -4,6 +4,6 @@ extends Sprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var adjacent = (self.position.x+(global.player.position.x-(get_viewport_rect().size.x/2))-global.world.base.position.x)
-	var opposite = (self.position.y+(global.player.position.y-(get_viewport_rect().size.y/2))-global.world.base.position.y)
-	rotation = cos(adjacent/opposite)
+	var adjacent = abs(global.player.position.x-global.world.base.position.x)
+	var hypotenuse = sqrt(((global.player.position.y-global.world.base.position.y)**2) + ((global.player.position.x-global.world.base.position.x)**2))
+	rotation = asin(adjacent/hypotenuse)
