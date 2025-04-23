89e74272101eb98d83160621e986b3242dfb49ba
GingerEdition22 <185860158+GingerEdition22@users.noreply.github.com>
2025-04-23 16:18:08 -0400

    Second iteration of control visuals

--- ControlVisuals/leftClickControl.gd
+++ ControlVisuals/leftClickControl.gd
@@ -5,13 +5,10 @@ var _tool = ""
 func _ready():
 	input.UI_Mouse.connect(_showUIMessage)
 	setTool(global.player.getMode())
-
-func changeText(_newText):
-	$Label.text = str(_newText)
 func setTool(_tool:String):
 	if len(_tool)>1:
 		_tool = utils.toolsJSON[_tool]["verb"]
-		changeText(_tool)
+		$Label.text = _tool
 func _showUIMessage(_value:bool):
 	if _value:
 		$Label.text = "Click"
